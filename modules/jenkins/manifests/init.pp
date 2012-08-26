50a9d636e2670631d2b7cc176585c2b9b4401980
Steve Messina <steve.g.messina@gmail.com>
2012-08-26 09:14:54 -0400

    moved jenkins install to /var/lib

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -4,15 +4,15 @@ class jenkins {
 		ensure => installed,
 	}
 
-	file { "/etc/jenkins":
+	file { "/var/lib/jenkins":
 		ensure => "directory",
 	}
 
-	file { "/etc/jenkins/plugins":
+	file { "/var/lib/jenkins/plugins":
 		ensure => "directory",
 	}
 	
-	file { "/etc/jenkins/jenkins.war":
+	file { "/var/lib/jenkins/jenkins.war":
 		source => "puppet:///modules/jenkins/jenkins.war",
 		ensure => "directory",
 		owner => "root",
@@ -20,49 +20,49 @@ class jenkins {
 		mode => 0555
 	}
 
-	file { "/etc/jenkins/plugins/ant.hpi":
+	file { "/var/lib/jenkins/plugins/ant.hpi":
 		source => "puppet:///modules/jenkins/plugins/ant.hpi",
 		ensure => "directory",
 		owner => "root",
 		group => "root"
 	}
 
-        file { "/etc/jenkins/plugins/git.hpi":
+        file { "/var/lib/jenkins/plugins/git.hpi":
                 source => "puppet:///modules/jenkins/plugins/git.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/github.hpi":
+        file { "/var/lib/jenkins/plugins/github.hpi":
                 source => "puppet:///modules/jenkins/plugins/github.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/maven-plugin.hpi":
+        file { "/var/lib/jenkins/plugins/maven-plugin.hpi":
                 source => "puppet:///modules/jenkins/plugins/maven-plugin.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/subversion.hpi":
+        file { "/var/lib/jenkins/plugins/subversion.hpi":
                 source => "puppet:///modules/jenkins/plugins/subversion.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/maven-dependency-update-trigger.hpi":
+        file { "/var/lib/jenkins/plugins/maven-dependency-update-trigger.hpi":
                 source => "puppet:///modules/jenkins/plugins/maven-dependency-update-trigger.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/maven-invoker-plugin.hpi":
+        file { "/var/lib/jenkins/plugins/maven-invoker-plugin.hpi":
                 source => "puppet:///modules/jenkins/plugins/maven-invoker-plugin.hpi",
                 ensure => "directory",
                 owner => "root",
@@ -70,53 +70,58 @@ class jenkins {
         }
 
 
-        file { "/etc/jenkins/plugins/svn-release-mgr.hpi":
+        file { "/var/lib/jenkins/plugins/svn-release-mgr.hpi":
                 source => "puppet:///modules/jenkins/plugins/svn-release-mgr.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/svnmerge.hpi":
+        file { "/var/lib/jenkins/plugins/svnmerge.hpi":
                 source => "puppet:///modules/jenkins/plugins/svnmerge.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/github-api.hpi":
+        file { "/var/lib/jenkins/plugins/github-api.hpi":
                 source => "puppet:///modules/jenkins/plugins/github-api.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/sonar.hpi":
+        file { "/var/lib/jenkins/plugins/sonar.hpi":
                 source => "puppet:///modules/jenkins/plugins/sonar.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/gerrit.hpi":
+        file { "/var/lib/jenkins/plugins/gerrit.hpi":
                 source => "puppet:///modules/jenkins/plugins/gerrit.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
-        file { "/etc/jenkins/plugins/gerrit-trigger.hpi":
+        file { "/var/lib/jenkins/plugins/gerrit-trigger.hpi":
                 source => "puppet:///modules/jenkins/plugins/gerrit-trigger.hpi",
                 ensure => "directory",
                 owner => "root",
                 group => "root"
         }
 
+	exec { "start_jenkins":
+		command => "nohup java -jar jenkins.war > /var/log/jenkins.log 2>&1",
+		path => "/var/lib/jenkins",
+	}
+
 
 	service { "jenkins":
 		ensure => running,
 		enable => true,
-		require => [ Package["jenkins"], File["/etc/jenkins/jenkins.war"] ],
+		require => [ Package["jenkins"], File["/var/lib/jenkins/jenkins.war"] ],
 	}
 
 }
